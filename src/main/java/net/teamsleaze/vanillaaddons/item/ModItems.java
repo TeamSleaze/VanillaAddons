6e2afe89ff33b55644b7f86d98c95e3c6de65cd5
Sleaze <sleazestiks@gmail.com>
2022-05-30 21:44:54 +0200

    Added Rose Gold Shield #5
    Signed-off-by: Sleaze <sleazestiks@gmail.com>

--- src/main/java/net/teamsleaze/vanillaaddons/item/ModItems.java
+++ src/main/java/net/teamsleaze/vanillaaddons/item/ModItems.java
@@ -13,6 +13,11 @@ public class ModItems {
             new Item(new FabricItemSettings().group(ItemGroup.MATERIALS))
     );
 
+    public static final Item ROSE_GOLD_SHIELD = Registry.register(Registry.ITEM, new Identifier(VanillaAddons.MOD_ID[0], "rose_gold_shield"),
+            new FabricShieldItem(new FabricItemSettings().maxDamage(2500).group(ItemGroup.COMBAT), 10, 13, ModItems.ROSE_GOLD_INGOT)
+    ); // FabricShieldItem(settings.maxDamage(durability), cooldownTicks, enchantability, repairItem)
+
+
     public static void registerModItems() {
         VanillaAddons.LOGGER.info(VanillaAddons.MOD_ID[1] + " Registering Mod Items");
     }
