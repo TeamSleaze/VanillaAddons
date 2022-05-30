0bedfd434ac4a642dad610bb9e45e7c6331e856e
Sleaze <sleazestiks@gmail.com>
2022-05-30 18:50:38 +0200

    Added Rose Gold Ingot #4
    Signed-off-by: Sleaze <sleazestiks@gmail.com>

--- src/main/java/net/teamsleaze/vanillaaddons/item/ModItems.java
+++ src/main/java/net/teamsleaze/vanillaaddons/item/ModItems.java
@@ -1,15 +1,17 @@
 package net.teamsleaze.vanillaaddons.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.teamsleaze.vanillaaddons.VanillaAddons;
 
 public class ModItems {
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registry.ITEM, new Identifier(VanillaAddons.MOD_ID[0], name), item);
-    }
+    public static Item ROSE_GOLD_INGOT = Registry.register(Registry.ITEM, new Identifier(VanillaAddons.MOD_ID[0], "rose_gold_ingot"),
+            new Item(new FabricItemSettings().group(ItemGroup.MATERIALS))
+    );
 
     public static void registerModItems() {
         VanillaAddons.LOGGER.info(VanillaAddons.MOD_ID[1] + " Registering Mod Items");
