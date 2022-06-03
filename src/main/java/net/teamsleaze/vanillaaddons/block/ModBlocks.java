46ac6c3c6b693f9508e03c594845afcb29c17f4d
Sleaze <sleazestiks@gmail.com>
2022-06-03 16:57:19 +0200

    Added more Storage Blocks (No Tex) - Stick Block - Chorus Fruit Block - Sugar Cane Block - Bamboo Block - Nether Wart Block - Cocoa Bean Block - Beetroot Block - Potato Block - Carrot Block - Apple Block - Golden Apple Block - Cactus Block
    Signed-off-by: Sleaze <sleazestiks@gmail.com>

--- src/main/java/net/teamsleaze/vanillaaddons/block/ModBlocks.java
+++ src/main/java/net/teamsleaze/vanillaaddons/block/ModBlocks.java
@@ -40,6 +40,20 @@ public class ModBlocks {
     public static Block TRIMMED_CRIMSON_PLANKS = new Block(AbstractBlock.Settings.of(Material.WOOD).resistance(3.0F).hardness(2.0F).sounds(BlockSoundGroup.WOOD));
     public static Block TRIMMED_WARPED_PLANKS = new Block(AbstractBlock.Settings.of(Material.WOOD).resistance(3.0F).hardness(2.0F).sounds(BlockSoundGroup.WOOD));
 
+    // STORAGE BLOCKS
+    public static Block STICK_BLOCK = new Block(AbstractBlock.Settings.of(Material.SOLID_ORGANIC, MapColor.BROWN).strength(0.5F).sounds(BlockSoundGroup.GRASS));
+    public static Block CHORUS_FRUIT_BLOCK = new Block(AbstractBlock.Settings.of(Material.SOLID_ORGANIC, MapColor.PURPLE).strength(0.5F).sounds(BlockSoundGroup.GRASS));
+    public static Block SUGAR_CANE_BLOCK = new Block(AbstractBlock.Settings.of(Material.SOLID_ORGANIC, MapColor.EMERALD_GREEN).strength(0.5F).sounds(BlockSoundGroup.GRASS));
+    public static Block BAMBOO_BLOCK = new Block(AbstractBlock.Settings.of(Material.SOLID_ORGANIC, MapColor.EMERALD_GREEN).strength(0.5F).sounds(BlockSoundGroup.GRASS));
+    public static Block NETHER_WART_BLOCK = new Block(AbstractBlock.Settings.of(Material.SOLID_ORGANIC, MapColor.DARK_RED).strength(0.5F).sounds(BlockSoundGroup.GRASS));
+    public static Block COCOA_BEAN_BLOCK = new Block(AbstractBlock.Settings.of(Material.SOLID_ORGANIC, MapColor.BROWN).strength(0.5F).sounds(BlockSoundGroup.GRASS));
+    public static Block BEETROOT_BLOCK = new Block(AbstractBlock.Settings.of(Material.SOLID_ORGANIC, MapColor.RED).strength(0.5F).sounds(BlockSoundGroup.GRASS));
+    public static Block POTATO_BLOCK = new Block(AbstractBlock.Settings.of(Material.SOLID_ORGANIC, MapColor.PALE_YELLOW).strength(0.5F).sounds(BlockSoundGroup.GRASS));
+    public static Block CARROT_BLOCK = new Block(AbstractBlock.Settings.of(Material.SOLID_ORGANIC, MapColor.ORANGE).strength(0.5F).sounds(BlockSoundGroup.GRASS));
+    public static Block APPLE_BLOCK = new Block(AbstractBlock.Settings.of(Material.SOLID_ORGANIC, MapColor.RED).strength(0.5F).sounds(BlockSoundGroup.GRASS));
+    public static Block GOLDEN_APPLE_BLOCK = new Block(AbstractBlock.Settings.of(Material.SOLID_ORGANIC, MapColor.GOLD).strength(0.5F).sounds(BlockSoundGroup.GRASS));
+    public static Block CACTUS_BLOCK = new Block(AbstractBlock.Settings.of(Material.SOLID_ORGANIC, MapColor.GREEN).strength(0.5F).sounds(BlockSoundGroup.GRASS));
+
 
     public static void registerModBlocks() {
         VanillaAddons.LOGGER.info(VanillaAddons.MOD_ID[1] + " Registering Mod Blocks");
@@ -81,6 +95,43 @@ public class ModBlocks {
 
         Registry.register(Registry.BLOCK, new Identifier(VanillaAddons.MOD_ID[0], "trimmed_warped_planks"), TRIMMED_WARPED_PLANKS);
         Registry.register(Registry.ITEM, new Identifier(VanillaAddons.MOD_ID[0], "trimmed_warped_planks"), new BlockItem(TRIMMED_WARPED_PLANKS, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+
+        // STORAGE BLOCKS
+        Registry.register(Registry.BLOCK, new Identifier(VanillaAddons.MOD_ID[0], "stick_block"), STICK_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(VanillaAddons.MOD_ID[0], "stick_block"), new BlockItem(STICK_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+
+        Registry.register(Registry.BLOCK, new Identifier(VanillaAddons.MOD_ID[0], "chorus_fruit_block"), CHORUS_FRUIT_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(VanillaAddons.MOD_ID[0], "chorus_fruit_block"), new BlockItem(CHORUS_FRUIT_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+
+        Registry.register(Registry.BLOCK, new Identifier(VanillaAddons.MOD_ID[0], "sugar_cane_block"), SUGAR_CANE_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(VanillaAddons.MOD_ID[0], "sugar_cane_block"), new BlockItem(SUGAR_CANE_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+
+        Registry.register(Registry.BLOCK, new Identifier(VanillaAddons.MOD_ID[0], "bamboo_block"), BAMBOO_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(VanillaAddons.MOD_ID[0], "bamboo_block"), new BlockItem(BAMBOO_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+
+        Registry.register(Registry.BLOCK, new Identifier(VanillaAddons.MOD_ID[0], "nether_wart_block"), NETHER_WART_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(VanillaAddons.MOD_ID[0], "nether_wart_block"), new BlockItem(NETHER_WART_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+
+        Registry.register(Registry.BLOCK, new Identifier(VanillaAddons.MOD_ID[0], "cocoa_bean_block"), COCOA_BEAN_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(VanillaAddons.MOD_ID[0], "cocoa_bean_block"), new BlockItem(COCOA_BEAN_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+
+        Registry.register(Registry.BLOCK, new Identifier(VanillaAddons.MOD_ID[0], "beetroot_block"), BEETROOT_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(VanillaAddons.MOD_ID[0], "beetroot_block"), new BlockItem(BEETROOT_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+
+        Registry.register(Registry.BLOCK, new Identifier(VanillaAddons.MOD_ID[0], "potato_block"), POTATO_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(VanillaAddons.MOD_ID[0], "potato_block"), new BlockItem(POTATO_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+
+        Registry.register(Registry.BLOCK, new Identifier(VanillaAddons.MOD_ID[0], "carrot_block"), CARROT_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(VanillaAddons.MOD_ID[0], "carrot_block"), new BlockItem(CARROT_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+
+        Registry.register(Registry.BLOCK, new Identifier(VanillaAddons.MOD_ID[0], "apple_block"), APPLE_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(VanillaAddons.MOD_ID[0], "apple_block"), new BlockItem(APPLE_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+
+        Registry.register(Registry.BLOCK, new Identifier(VanillaAddons.MOD_ID[0], "golden_apple_block"), GOLDEN_APPLE_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(VanillaAddons.MOD_ID[0], "golden_apple_block"), new BlockItem(GOLDEN_APPLE_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
+
+        Registry.register(Registry.BLOCK, new Identifier(VanillaAddons.MOD_ID[0], "cactus_block"), CACTUS_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(VanillaAddons.MOD_ID[0], "cactus_block"), new BlockItem(CACTUS_BLOCK, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
     }
 
 }
